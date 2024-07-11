94df836c94fca68585e8798a52aec4cb7416eb90
nonyx777 <nahomretebo@gmail.com>
2024-07-11 10:03:30 +0300

    crud operations on the database

---
+++ client_service/app/crud.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_client(db: Session, client_id: int):
+    return db.query(models.Client).filter(models.Client.id == client_id).first()
+
+def get_clients(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Client).offset(skip).limit(limit).all()
+
+def create_client(db: Session, client: schemas.ClientCreate):
+    db_client = models.Client(**client.dict())
+    db.add(db_client)
+    db.commit()
+    db.refresh(db_client)
+    return db_client
