d3d75981050babfc45655f830ce92b778a635510
Bete77 <betemariamabenet8468@gmail.com>
2024-07-12 14:37:25 +0300

    created and dockerized react application

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+function App() {
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/clients")
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+
+  return <>Initial Setup</>;
+}
+
+export default App;
